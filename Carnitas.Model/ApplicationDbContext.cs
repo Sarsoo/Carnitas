005c74ac83e1ba1d543318f8c8c81a156193ad61
Andy Pack <andy@sarsoo.xyz>
2026-09-20 12:32:57 +0100

    adding grpc project, fixing db builder for instance settings

--- Carnitas.Model/ApplicationDbContext.cs
+++ Carnitas.Model/ApplicationDbContext.cs
@@ -62,6 +62,8 @@ public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
     {
         base.OnModelCreating(builder);
 
+        builder.BuildInstanceSettings();
+
         builder.BuildOrganisation()
             .BuildRepository()
             .BuildCheckout();
