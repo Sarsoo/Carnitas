ab7d95c04c1ff6a3006cda3aa14530ff3acc95f6
Andy Pack <andy@sarsoo.xyz>
2026-09-20 13:29:02 +0100

    add org settings tab

--- Carnitas.Web/Model/NewOrganisation.cs
+++ Carnitas.Web/Model/NewOrganisation.cs
@@ -7,4 +7,6 @@ public class NewOrganisation
     [Required]
     [StringLength(50, ErrorMessage = "Name must be at least 1 character long.", MinimumLength = 1)]
     public string Name { get; set; }
+
+    public string? Description { get; set; }
 }
